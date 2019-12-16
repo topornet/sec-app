257abb0e568ca1f25b3837bd89e63c213903a73a
Marco Baz <Marco.Baz.br@gmail.com>
2019-12-16 20:16:24 -0300

    Criado tela inicial com temporizador para abrir tela de login

--- app/src/main/java/com/happycode/sec_app/MainActivity.java
+++ app/src/main/java/com/happycode/sec_app/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import com.happycode.sec_app.ui.login.LoginActivity;
 
 public class MainActivity extends Activity {
 
