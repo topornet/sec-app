a90fa01013c5b1489445525896c123a8657d8a9a
Marco Baz <Marco.Baz.br@gmail.com>
2020-01-17 20:42:16 -0300

    Criada a tela de Login.

--- app/src/main/java/com/happycode/sec_app/LoginActivity.java
+++ app/src/main/java/com/happycode/sec_app/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.happycode.sec_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +13,25 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
+    @Override
+    protected void  onResume() {
+        super.onResume();
+
+        Button telaSolicitacao = (Button)findViewById(R.id.btnEntrar);
+
+        telaSolicitacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+
+                Intent intent = new Intent(LoginActivity.this, SolicitacaoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
 }
