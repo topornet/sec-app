35a362ff26c0b3a334d9d80bad93dae7f2b8eaca
lvolc <laerciolv@gmail.com>
2020-01-16 23:27:35 -0300

    acrescentada a lista de usuários fake na tela de compra

--- app/src/main/java/com/happycode/sec_app/CompraActivity.java
+++ app/src/main/java/com/happycode/sec_app/CompraActivity.java
@@ -5,14 +5,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class CompraActivity extends Activity {
+    private ListView listaUsuarios;
+    private String  [] usuarios = {
+            "usuario1 - 10",
+            "usuario2 - 20",
+            "usuario3 - 40",
+            "usuario4 - 15",
+            "usuario5 - 17",
+            "usuario6 - 26"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compra);
+
+        listaUsuarios = (ListView) findViewById(R.id.list_item);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                usuarios
+        );
+
+        listaUsuarios.setAdapter(adapter);
+
     }
 
 
